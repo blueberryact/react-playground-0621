d32f89e47b3cb96b9cfdd5501d83c425b77502f0
sjndowair <wodnjsdl1125@naver.com>
2024-06-26 22:59:09 +0900

    교체 없음 코드 리뷰 중 재작성

--- src/components/editor/EditorContainer.tsx
+++ src/components/editor/EditorContainer.tsx
@@ -2,18 +2,18 @@ import styled from "styled-components";
 import { ILayout } from "../../types/layouts";
 
 const Container = styled.section`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    padding: 1rem;
-    padding-top: 6rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  padding: 1rem;
+  padding-top: 6rem;
 `;
 
 const EditorContainer = ({ children }: ILayout) => {
-    return <Container>{children}</Container>;
+  return <Container>{children}</Container>;
 };
 
 export default EditorContainer;
--- src/context/themeCtx.tsx
+++ src/context/themeCtx.tsx
@@ -1,84 +1,84 @@
 import {
-    FC,
-    ReactNode,
-    createContext,
-    useCallback,
-    useContext,
-    useMemo,
-    useReducer,
+  FC,
+  ReactNode,
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useReducer,
 } from "react";
 
 interface IThemeState {
-    isDark: boolean;
+  isDark: boolean;
 }
 
 const initialState: IThemeState = {
-    isDark: false,
+  isDark: false,
 };
 
 type TThemeAction = {
-    type: "TOGGLE_THEME";
+  type: "TOGGLE_THEME";
 };
 
 export const ThemeContext = createContext<IThemeState | any>(initialState);
 ThemeContext.displayName = "themeContext";
 
 const themeReducer = (
-    state: IThemeState,
-    action: TThemeAction
+  state: IThemeState,
+  action: TThemeAction
 ): IThemeState => {
-    switch (action.type) {
-        case "TOGGLE_THEME":
-            return { isDark: !state.isDark };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "TOGGLE_THEME":
+      return { isDark: !state.isDark };
+    default:
+      return state;
+  }
 };
 
 export const ThemeModeProvider: FC<{
-    children?: string | ReactNode | JSX.Element;
+  children?: string | ReactNode | JSX.Element;
 }> = (props) => {
-    const [state, dispatch] = useReducer(themeReducer, initialState);
+  const [state, dispatch] = useReducer(themeReducer, initialState);
 
-    const toggleTheme = useCallback(
-        () => dispatch({ type: "TOGGLE_THEME" }),
-        [dispatch]
-    );
+  const toggleTheme = useCallback(
+    () => dispatch({ type: "TOGGLE_THEME" }),
+    [dispatch]
+  );
 
-    // const toggleTheme = useCallback(() => {
-    //     return dispatch({ type: "TOGGLE_THEME" });
-    // }, [dispatch]);
+  // const toggleTheme = useCallback(() => {
+  //     return dispatch({ type: "TOGGLE_THEME" });
+  // }, [dispatch]);
 
-    const value = useMemo(
-        () => ({ ...state, toggleTheme }),
-        [state, toggleTheme]
-    );
-    // const value = useMemo(() => {
-    //     return {
-    //         isDark: state.isDark,
-    //         toggleTheme: toggleTheme
-    //     }
-    // }, [])
+  const value = useMemo(
+    () => ({ ...state, toggleTheme }),
+    [state, toggleTheme]
+  );
+  // const value = useMemo(() => {
+  //     return {
+  //         isDark: state.isDark,
+  //         toggleTheme: toggleTheme
+  //     }
+  // }, [])
 
-    return <ThemeContext.Provider value={value} {...props} />;
+  return <ThemeContext.Provider value={value} {...props} />;
 };
 
 interface IUseTheme extends IThemeState {
-    toggleTheme(): void;
+  toggleTheme(): void;
 }
 
 export const useTheme = () => {
-    const ctx = useContext<IUseTheme>(ThemeContext);
+  const ctx = useContext<IUseTheme>(ThemeContext);
 
-    if (!ctx) {
-        throw new Error("useTheme must be used within a ThemeProvider");
-    }
+  if (!ctx) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
 
-    return ctx;
+  return ctx;
 };
 
 export const ManagedThemeProvider: FC<{ children?: ReactNode }> = ({
-    children,
+  children,
 }) => {
-    return <ThemeModeProvider>{children}</ThemeModeProvider>;
+  return <ThemeModeProvider>{children}</ThemeModeProvider>;
 };
